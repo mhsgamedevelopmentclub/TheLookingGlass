adf550888a96ddef072e06aafeda0c8afd6f9f6c
soup-5 <93955560+Soup-5@users.noreply.github.com>
2023-01-10 10:17:53 -0800

    setup testing scene + basic movement for retro player

--- testing/player.gd
+++ testing/player.gd
@@ -1,4 +1,20 @@
 extends KinematicBody2D
 
-export var init_speed := 10
-export var max_speed := 60
+const GRAV_CONSTANT = 19.6
+
+export var init_speed := 10.0
+export var max_speed := 60.0
+
+var velo = Vector2()
+
+func _physics_process(delta):
+	velo.y += delta * GRAV_CONSTANT
+	
+	if Input.is_action_pressed("move_right"):
+		velo.x = max_speed
+	elif Input.is_action_pressed("move_left"):
+		velo.x = -max_speed
+	else:
+		velo.x = 0
+	
+	move_and_slide(velo)
