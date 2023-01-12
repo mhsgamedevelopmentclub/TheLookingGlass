a8a0e0264f4d4fad59498b3b369e9278a1a38717
Soup-5 <93955560+Soup-5@users.noreply.github.com>
2023-01-12 15:39:06 -0800

    added attempt of variable jump

--- testing/player.gd
+++ testing/player.gd
@@ -4,8 +4,8 @@ const GRAV_CONSTANT = 400
 
 export var jump_height := 200.0
 export var max_speed := 120.0
-export var acceleration := 0.02
-export var deceleration := 0.08
+export var acceleration := 0.04
+export var deceleration := 0.04
 
 var velo = Vector2()
 
@@ -21,6 +21,9 @@ func _physics_process(delta):
 		velo.x = lerp(velo.x, 0, deceleration)
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velo.y = -(jump_height)
+		var start = OS.get_ticks_msec()
+		if Input.is_action_just_released("jump"):
+			var end = OS.get_ticks_msec() - start
+			velo.y = -(jump_height * end * 100)
 	
 	move_and_slide(velo, Vector2.UP)
