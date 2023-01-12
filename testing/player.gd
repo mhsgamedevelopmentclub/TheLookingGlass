fd5d43fa0d1f508bbc2e1a83e098a49f55da7347
Soup-5 <93955560+Soup-5@users.noreply.github.com>
2023-01-12 14:26:38 -0800

    Added acceleration/deceleration to player movement

--- testing/player.gd
+++ testing/player.gd
@@ -1,20 +1,26 @@
 extends KinematicBody2D
 
-const GRAV_CONSTANT = 19.6
+const GRAV_CONSTANT = 400
 
-export var init_speed := 10.0
-export var max_speed := 60.0
+export var jump_height := 200.0
+export var max_speed := 120.0
+export var acceleration := 0.02
+export var deceleration := 0.08
 
 var velo = Vector2()
 
 func _physics_process(delta):
-	velo.y += delta * GRAV_CONSTANT
+	if !is_on_floor():
+		velo.y += delta * GRAV_CONSTANT
 	
 	if Input.is_action_pressed("move_right"):
-		velo.x = max_speed
+		velo.x = lerp(velo.x, max_speed, acceleration)
 	elif Input.is_action_pressed("move_left"):
-		velo.x = -max_speed
+		velo.x = lerp(velo.x, -max_speed, acceleration)
 	else:
-		velo.x = 0
+		velo.x = lerp(velo.x, 0, deceleration)
 	
-	move_and_slide(velo)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velo.y = -(jump_height)
+	
+	move_and_slide(velo, Vector2.UP)
