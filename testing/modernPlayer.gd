a3edc2de47be486b020eeaf2f008f12118bf2633
Soup-5 <93955560+Soup-5@users.noreply.github.com>
2023-01-19 15:34:44 -0800

    Camera & Modern Player
    - Added camera component to both players
    - Created (initial) modern player movement script

---
+++ testing/modernPlayer.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+const GRAV_CONSTANT := 400
+
+export var jump_height := 200.0
+export var max_speed := 120.0
+export var acceleration := 0.06
+export var deceleration := 0.06
+
+var velo = Vector2()
+var is_jumping := false
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right"):
+		velo.x = lerp(velo.x, max_speed, acceleration)
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("move_left"):
+		velo.x = lerp(velo.x, -max_speed, acceleration)
+		$AnimatedSprite.flip_h = true
+	else:
+		velo.x = lerp(velo.x, 0, deceleration)
+	
+	if not is_on_floor():
+		velo.y += delta * GRAV_CONSTANT
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velo.y = -jump_height
+		is_jumping = true
+	if Input.is_action_just_released("jump") and is_jumping:
+		velo.y = -25
+		is_jumping = false
+	
+	if velo.x != 0:
+		$AnimatedSprite.play()
+	
+	move_and_slide(velo, Vector2.UP)
