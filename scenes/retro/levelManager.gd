ade2fef45e091ec0eec1bf182d95b4ac28f93822
Soup-5 <93955560+Soup-5@users.noreply.github.com>
2023-03-16 15:36:23 -0700

    updated stuff

---
+++ scenes/retro/levelManager.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+"""
+	Main purpose of scene is to recieve signals
+	from other game objects for level changing
+	*DO NOT DEFINE LEVEL FUNCTIONS IN HERE*
+"""
+
+func swap_world():
+	get_tree().change_scene("res://scenes/modern/modernTest.tscn")
+
+func level_complete():
+	pass
